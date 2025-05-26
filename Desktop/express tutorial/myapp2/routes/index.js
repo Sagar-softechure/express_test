730f875ff9ccdcfcc1e55ddace783bbbad2254f7
Sagar-softechure <136797867+Sagar-softechure@users.noreply.github.com>
2025-05-26 19:22:20 +0530

    changes

--- Desktop/express tutorial/myapp2/routes/index.js
+++ Desktop/express tutorial/myapp2/routes/index.js
@@ -1,14 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-var userdata = [
-  {name: 'John', age: 20},
-  {name: 'Jane', age: 21},
-  {name: 'Jim', age: 22}
-]
-
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'hello world', userdata: userdata });
+  res.render('index', { title: 'hello world' });
   // res.send('Hello user');
 });
 
--- Desktop/express tutorial/myapp2/routes/users.js
+++ Desktop/express tutorial/myapp2/routes/users.js
@@ -13,5 +13,17 @@ router.get('/',(req, res) => {
   res.send('search not found2');
 });
 
+var userdata = [
+  {name: 'John', age: 20},
+  {name: 'Jane', age: 21},
+  {name: 'Jim', age: 22}
+]
+
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'hello world', userdata: userdata });
+  // res.send('Hello user');
+});
+
+
 
 module.exports = router;
